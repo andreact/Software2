2b9be57114e8e315faffc3bc7b7c25f4d06e5b8b
andreact <andrea.c.t.1996@gmail.com>
2018-04-14 11:03:04 -0500

    Reemplazo de la carpeta correcta

---
+++ prelex/src/main/java/com/example/prelex/PrelexApplication.java
@@ -0,0 +1,12 @@
+package com.example.prelex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrelexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrelexApplication.class, args);
+	}
+}
