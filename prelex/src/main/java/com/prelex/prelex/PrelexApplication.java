34a90de3f8e9e58d56980600dbe4dadf5cf7e791
AngeyVelez <angey.1701316312@ucaldas.edu.co>
2018-04-14 09:51:56 -0500

    estructura proyecto spring

---
+++ prelex/src/main/java/com/prelex/prelex/PrelexApplication.java
@@ -0,0 +1,12 @@
+package com.prelex.prelex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrelexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrelexApplication.class, args);
+	}
+}
